939a58ea652db4f9b95f7507a041f1a43479833a
eklam <lamethan512@gmail.com>
2025-11-20 13:14:46 -0600

    poiuuyt

--- Project3/js/scripts.js
+++ Project3/js/scripts.js
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (questionBtn) {
         questionBtn.addEventListener("click", () => {
-            alert("If you have questions, contact me at:\nYOUR_EMAIL@HERE.COM");
+            alert("If you have questions, contact me at:\neklam@cps.edu");
         });
     }
 });    
